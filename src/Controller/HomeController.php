8292c2ca9f0148a7af49d0c974423878e4de30fe
Varesva <tavaresjustine.pro@mail.com>
2022-04-27 13:43:34 +0200

    first

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="app_home")
+     * @Route("/", name="app_home")
      */
     public function index(): Response
     {       
