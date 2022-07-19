8ee3944c51ef2eb563e65ea0844176bf8e3269d2
Varesva <tavaresjustine.pro@mail.com>
2022-07-19 20:01:38 +0200

    heberg

---
+++ public/page-down.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Votre site Roots revient très vite ! Nous sommes désolés !
+			">
+	<meta name="keywords" content="restaurant, africain, antillais, caribéen, livre, recette, paris">
+	<meta name="author" content="Justine TAVARES VAZ">
+
+	{# CSS #}
+	<link rel="stylesheet" href="../public/css/fonts.css">
+	<link rel="stylesheet" href="../public/css/style.css">
+
+
+	{# GOOGLE FONTS #}
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+
+	<title>
+		Mode maintenance - Roots
+	</title>
+</head>
+
+<body>
+	<main class="site_container">
+		<h1>Roots est en mode maintenance</h1>
+
+		<p>Nous vous prions de bien vouloir nous excuser ! </p>
+		<p>Nous reviendrons très vite !</p>
+		<figure>
+			<img src="../public/img/page-down.png" alt="Le site Roots est en mode maintenance">
+		</figure>
+	</main>
+</body>
+
+</html>
